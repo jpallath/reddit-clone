c2b1f675150b32350de2b9bb85a2b92272259bc1
Jerry Pallath <jpallath@nyu.edu>
2018-04-18 17:10:24 -0400

    cors

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import routes from "./routes";
+import cors from "cors";
 
 mongoose.connect("mongodb://localhost:27017/redditclone", () => {
   console.log("connected to mongodb");
@@ -9,6 +10,7 @@ mongoose.connect("mongodb://localhost:27017/redditclone", () => {
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api", routes);
